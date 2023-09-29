9fa68a73c8754b3afb2b124dccf5cb9467677b82
sparsh choudhary <sparsh.choudhary@simplifyvms.com>
2023-09-29 15:10:05 +0530

    update

--- lib/widgets/markdown.dart
+++ lib/widgets/markdown.dart
@@ -14,36 +14,58 @@ class _MarkDownState extends State<MarkDown> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: FutureBuilder(
-          future: rootBundle.loadString("/doc/README.md"),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.hasData) {
-              return Markdown(
-                data: snapshot.data!,
-                styleSheet: MarkdownStyleSheet(
-                  h1: TextStyle(color: Colors.green),
-                  h2: TextStyle(color: Colors.green),
-                  h3: TextStyle(color: Colors.purpleAccent),
-                  code: TextStyle(
-                    color: Colors.pinkAccent,
-                    backgroundColor: Colors.transparent,
-                  ),
-                  codeblockDecoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(12.0),
-                    border: Border.all(
-                      color: Colors.black.withOpacity(0.2),
-                      width: 0.6,
-                    ),
-                  ),
-                ),
-              );
-            }
+      body: Markdown(
+        data: "## Cred Widget Library",
+        // data: snapshot.data!,
+        styleSheet: MarkdownStyleSheet(
+          h1: TextStyle(color: Colors.green),
+          h2: TextStyle(color: Colors.green),
+          h3: TextStyle(color: Colors.purpleAccent),
+          code: TextStyle(
+            color: Colors.pinkAccent,
+            backgroundColor: Colors.transparent,
+          ),
+          codeblockDecoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(12.0),
+            border: Border.all(
+              color: Colors.black.withOpacity(0.2),
+              width: 0.6,
+            ),
+          ),
+        ),
+      ),
+      // body: FutureBuilder(
+      //     future: rootBundle.loadString("/doc/README.md"),
+      //     builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+      //       if (snapshot.hasData) {
+      //         return Markdown(
+      //           data: "## Cred Widget Library",
+      //           // data: snapshot.data!,
+      //           styleSheet: MarkdownStyleSheet(
+      //             h1: TextStyle(color: Colors.green),
+      //             h2: TextStyle(color: Colors.green),
+      //             h3: TextStyle(color: Colors.purpleAccent),
+      //             code: TextStyle(
+      //               color: Colors.pinkAccent,
+      //               backgroundColor: Colors.transparent,
+      //             ),
+      //             codeblockDecoration: BoxDecoration(
+      //               color: Colors.grey.withOpacity(0.1),
+      //               borderRadius: BorderRadius.circular(12.0),
+      //               border: Border.all(
+      //                 color: Colors.black.withOpacity(0.2),
+      //                 width: 0.6,
+      //               ),
+      //             ),
+      //           ),
+      //         );
+      //       }
 
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }),
+      //       return Center(
+      //         child: CircularProgressIndicator(),
+      //       );
+      //     }),
     );
   }
 }
